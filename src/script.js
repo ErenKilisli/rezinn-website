ebec52addb5db91be382df8c6e1d5b28a9dd5224
Ibrahim Eren Kilisli <ierenkilisli@gmail.com>
2025-04-30 19:20:59 +0300

    footer animation

--- src/script.js
+++ src/script.js
@@ -1,1 +1,23 @@
-console.log("Rezinn landing page loaded.");
+const lines = [
+    "Şehrin en güzel restoranında",
+    "Hafta sonu buluşmasında",
+    "Gecenin en popüler mekanında",
+    "Arkadaşlarla eğlenmek istediğinde"
+  ];
+  
+  let index = 0;
+  const changingLine = document.getElementById("changing-line");
+  
+  function changeLine() {
+    changingLine.style.opacity = 0;
+  
+    setTimeout(() => {
+      changingLine.innerText = lines[index];
+      changingLine.style.opacity = 1;
+      index = (index + 1) % lines.length;
+    }, 500);
+  }
+  
+  changeLine();
+  setInterval(changeLine, 3000);
+  
